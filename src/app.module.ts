0f3b6bfb8095abd8db17061c8c92ef2bed17f7fc
Dan <wambu43@gmail.com>
2022-09-03 13:27:42 +0300

    Added customers controller, service, config validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CustomersController } from './customers/customers.controller';
+import { CustomersService } from './customers/customers.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    validationSchema: Joi.object({
+      APP_PORT: Joi.number().required(),
+      POSTGRES_HOST: Joi.string().required(),
+      POSTGRES_PORT: Joi.number().required(),
+      POSTGRES_USER: Joi.string().required(),
+      POSTGRES_PASSWORD: Joi.string().required().allow(null,''),
+      POSTGRES_DB: Joi.string().required(),
+    })
+  })],
+  controllers: [AppController, CustomersController],
+  providers: [AppService, CustomersService],
 })
 export class AppModule {}
